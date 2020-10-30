62f31762dba8df35ab29a4b968c2eb4f51915e59
Ivan Simunovic <ivan.simunovic9898@gmail.com>
2020-10-30 12:34:26 +0100

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Ivan Simunovic on 30/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
